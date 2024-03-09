41125e242dbd6fa2f3cdf6496e786cb960842fa4
Víctor Buralla Blasco <vburalla@mercadona.es>
2024-03-09 17:58:00 +0100

    first solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -9,7 +9,26 @@ public class Solution {
             Integer[] nums,
             Integer target) {
 
-        return null;
+        int result = -1;
+        if(nums.length > 0) {
+            int left = 0;
+            int right = nums.length -1;
+            int middle = 0;
+            while(left < right - 1) {
+                middle = left + ((right - left) / 2);
+                if (nums[middle] > target) {
+                    right = middle;
+                } else {
+                    left = middle;
+                }
+            }
+            if(nums[left].equals(target))
+                result = left;
+            else if(nums[right].equals(target))
+                result = right;
+        }
+
+        return result;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,12 +3,19 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @Slf4j
 class SolutionTest {
 
     @Test
     void testSolution() {
 
+        Solution solution = new Solution();
+        Integer[] search = new Integer[]{-1,0,3,5,9,12};
+        var a = solution.search(search, 9);
+
+        assertEquals(4, a);
         log.info("Don't forget to write the tests to make sure it works. Enjoy!");
     }
 
